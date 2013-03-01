5be110186f0ff14a13a22eef970ca18087026511
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2013-03-01 15:14:53 +0000

    Organização em packages:
    contantValues -> Values.java -> contem as constantes pa evitar 'Magic
    numbers'

---
+++ Backup Service/src/constantValues/Values.java
@@ -0,0 +1,13 @@
+package constantValues;
+
+
+import java.net.*;
+
+
+
+public class Values{
+	
+	public static final int multicast_group_port = 10001;
+	public static final String multicast_group_address = "225.0.0.1";
+	
+}
